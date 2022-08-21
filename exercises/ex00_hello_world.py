5ac5aa3166a725873d5eac35df9ff22e66fd1689
Andrea Gil-Munoz <adaniela@ad.unc.edu>
2022-08-21 00:10:53 -0400

    "Finished Exercise 00!”

--- exercises/ex00_hello_world.py
+++ exercises/ex00_hello_world.py
@@ -1,11 +1,3 @@
 """My first program for COMP110."""
-__author__ = "730483450"
-NAMED_CONSTANT: int = 1 
-def add_one(num: int) -> int:
-    """Adds one to a given number"""
-    return num + NAMED_CONSTANT
-def print_Hello(Hello: str) -> None:
-    """Prints out Hello."""
-print ("Hello")
-def print_World(World: str) -> None:
-    """Prints out world."""
+__author__= "730483450"
+print("Hello, World.")
