5b88f8ecd383abfb382cb2fd2a14d7fb989b9e85
Andrea Gil-Munoz <adaniela@ad.unc.edu>
2022-08-29 18:02:38 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -10,23 +10,26 @@ single_letter: str = input("Enter a single character:")
 if len(single_letter) != 1:
     print("Error: Character must be a single character.")
     exit()
-count = 0
 print("Searching for " + single_letter + " in " + five_letter_word)
-
+count = int = 0
 if single_letter == five_letter_word[0]:
     print(single_letter + " found at index 0")
-count = count +1
+    count = count + 1
 if five_letter_word[1] == single_letter:
     print(single_letter + " found at index 1")
-count = count + 1 
+    count = count + 1
 if five_letter_word[2] == single_letter:
     print(single_letter + " found at index 2")
-count = count + 1 
+    count = count + 1
 if five_letter_word[3] == single_letter:
     print(single_letter + " found at index 3")
-count = count + 1 
+    count = count + 1
 if five_letter_word[4] == single_letter:
     print(single_letter + " found at index 4")
-count = count + 1 
-
-print(str(count) + " instances of " + single_letter + " found in " + five_letter_word)
+    count = count + 1
+if count==1:
+    print(str(count) + " instance of " + single_letter + " found in " + five_letter_word)
+if count >1:
+    print(str(count) + " instance of " + single_letter + " found in " + five_letter_word)
+if count == 0:
+    print(" No instances of " + single_letter + " found in " + five_letter_word)
