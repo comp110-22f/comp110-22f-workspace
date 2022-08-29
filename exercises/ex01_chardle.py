33d833d62d680c18fb0fe2b67f2256998cbd86c3
Andrea Gil-Munoz <adaniela@ad.unc.edu>
2022-08-29 14:32:57 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,30 +1,38 @@
 """"EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730483450"
 
-Word: str= input ("Enter a 5-character word:") 
-letter: str= input ("Enter a single character:")
-print ("Searching for " + letter + " in " + Word)
-str=[0]==1
-if Word[0] == letter:
-    print(letter + "found at index 0")
-str=[1]==1
-if Word[1] == letter:
-    print(letter + "found at index 1")
-str=[2]==2
-if Word[2] == letter:
-     print(letter + "found at index 2")
-str=[3]==3
-if Word[3] == letter:
-      print(letter + "found at index 3")
-str=[4]==4
-if Word[4] == letter:
-       print(letter + "found at index 4")
-else:
-    if len(Word) != 5:
-        print ("Error: Word must contain 5 characters")
-        if len(letter) != 1:
-             print ("Error: Character must be a single character")
-print("Error: Word must contain 5 characters")
+word: str= input ("Enter a 5-character word:") 
+if len(word) != 5:
+    print ("Error: Word must contain 5 characters.")
+    exit()
 
-len(letter) != 1
-print("Error: Character must be a single character")
+letter: str= input ("Enter a single character:")
+if len(letter) != 1:
+    print ("Error: Character must be a single character")
+    exit()
+
+print ("Searching for " + letter + " in " + word)
+
+count = 0
+
+if word[0] == letter: 
+    print(letter + "found at index 0")
+count= count + 1 
+
+if word[1] == letter:
+    print(letter + "found at index 1")
+count= count + 1 
+
+if word[2] == letter:
+     print(letter + "found at index 2")
+count= count+ 1 
+
+if word[3] == letter:
+    print(letter + "found at index 3")
+count= count + 1 
+
+if word[4] == letter:
+    print(letter + "found at index 4")
+count= count + 1 
+
+print(str(count) + "instance of" + letter + "found in" + word)
