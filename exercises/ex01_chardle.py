fe6c6183ec669155331c105ebad93feac0c7fb51
Hannah Reed <hannahrr@unc.edu>
2022-08-29 01:11:49 -0400

    final ex01_chardle

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,43 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__HannahReed__ = "730557115"
+
+greeting: str = str(input("Enter a 5-character word: "))
+
+if len(greeting) > 5: 
+    print("Error: Word must contain 5 characters")
+    exit()
+if len(greeting) < 5: 
+    print("Error: Word must contain 5 characters")
+    exit()
+    
+letter: str = str(input("Enter a single character: "))
+
+print("Searching for " + letter + " in "  + greeting)
+
+counter_man = 0  
+
+if len(letter) > 1: 
+    print("Error: Character must be a single character")
+    exit()
+
+if letter == greeting[0]:
+    print(letter  + " found at index 0")
+    counter_man = counter_man + 1 
+if letter == greeting[1]:
+    print(letter  + " found at index 1")
+    counter_man = counter_man + 1 
+if letter == greeting[2]:
+    print(letter  + " found at index 2")
+    counter_man = counter_man + 1 
+if letter == greeting[3]:
+    print(letter  + " found at index 3")
+    counter_man = counter_man + 1 
+if letter == greeting[4]:
+    print(letter  + " found at index 4")
+    counter_man = counter_man + 1 
+
+if counter_man == 1: print(str(counter_man) + " instance of " + letter + " found in " + greeting) 
+if counter_man >1: print(str(counter_man) + " instances of " + letter + " found in " + greeting) 
+if counter_man == 0: print("No instances of " + letter  + " found in " +  greeting)
+
