61ded63cceb17fc51c848db9ab413647ee657343
Andrea Gil-Munoz <adaniela@ad.unc.edu>
2022-08-29 14:54:15 -0400

    Progress on Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -1,38 +1,38 @@
 """"EX01 - Chardle - A cute step toward Wordle."""
 __author__ = "730483450"
 
-word: str= input ("Enter a 5-character word:") 
+word: str = input("Enter a 5-character word:") 
 if len(word) != 5:
-    print ("Error: Word must contain 5 characters.")
+    print("Error: Word must contain 5 characters.")
     exit()
 
-letter: str= input ("Enter a single character:")
+letter: str = input("Enter a single character:")
 if len(letter) != 1:
-    print ("Error: Character must be a single character")
+    print("Error: Character must be a single character")
     exit()
 
-print ("Searching for " + letter + " in " + word)
+print("Searching for " + letter + " in " + word)
 
 count = 0
 
 if word[0] == letter: 
     print(letter + "found at index 0")
-count= count + 1 
+count = count + 1 
 
 if word[1] == letter:
     print(letter + "found at index 1")
-count= count + 1 
+count = count + 1 
 
 if word[2] == letter:
      print(letter + "found at index 2")
-count= count+ 1 
+count = count+ 1 
 
 if word[3] == letter:
     print(letter + "found at index 3")
-count= count + 1 
+count = count + 1 
 
 if word[4] == letter:
     print(letter + "found at index 4")
-count= count + 1 
+count = count + 1 
 
 print(str(count) + "instance of" + letter + "found in" + word)
