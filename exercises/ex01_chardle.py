286e2596e101324b08364aefe5e1155ba7878a24
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-04 17:16:06 -0400

    done

---
+++ exercises/ex01_chardle.py
@@ -0,0 +1,12 @@
+"""EX01 - Chardle - A cute step toward Wordle."""
+
+__author__ = "730470086"
+
+word: str = input("Enter a 5-character word: ")
+character: str = input("Enter a single character: ")
+print("Searching for " + character + " in " + word)
+if character == word[0, 1, 2, 3, 4]:
+    print(character + "found at index 1")
+
+word: str = input("Enter a 5-character word:")
+character: str = input("Enter a single character: ")
