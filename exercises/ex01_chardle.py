ae08e43573b4edcaf7d7860f31bfbf55f94a0719
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-04 23:46:30 -0400

    hi

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -3,10 +3,21 @@
 __author__ = "730470086"
 
 word: str = input("Enter a 5-character word: ")
-character: str = input("Enter a single character: ")
-print("Searching for " + character + " in " + word)
-if character == word[0, 1, 2, 3, 4]:
-    print(character + "found at index 1")
+if word != len(4):
+    exit( print("Error: Word must contain 5 characters")) 
 
-word: str = input("Enter a 5-character word:")
 character: str = input("Enter a single character: ")
+if character != len(0):
+    exit( print("Error: Character must be a single character")) 
+print("Searching for " + character + " in " + word)
+
+if character == word [0]:
+    print(character + " found at index 0")
+if character == word [1]:
+    print(character + " found at index 1")
+if character == word [2]:
+    print(character + " found at index 2")
+if character == word [3]:
+    print(character + " found at index 3")
+if character == word [4]:
+    print(character + " found at index 4")
