be3de406d760c5479c53f4c94a21c649df857e2e
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-09 20:34:58 -0400

    yo

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -8,37 +8,37 @@ counter = 0
 
 word: str = input("Enter a 5-character word: ")
 if len(word) != len(secret):
-    exit( print("Error: Word must contain 5 characters")) 
+    exit(print("Error: Word must contain 5 characters")) 
 
 character: str = input("Enter a single character: ")
 if len(character) != len(compare):
-    exit( print("Error: Character must be a single character."))
+    exit(print("Error: Character must be a single character."))
 else:
     print("Searching for " + character + " in " + word)
 
-if character == word [0]:
+if character == word[0]:
     print(character + " found at index 0")
     counter = counter + 1
-if character == word [1]:
+if character == word[1]:
     print(character + " found at index 1")
     counter = counter + 1
-if character == word [2]:
+if character == word[2]:
     print(character + " found at index 2")
     counter = counter + 1
-if character == word [3]:
+if character == word[3]:
     print(character + " found at index 3")
     counter = counter + 1
-if character == word [4]:
+if character == word[4]:
     print(character + " found at index 4")
     counter = counter + 1
     
 if counter == 0:
-    print("No instances of " + character + " found in " + word )
+    print("No instances of " + character + " found in " + word)
 else:
     if counter == 1:
-        print( str(counter) + " instance of " + character + " found in " + word)
+        print(str(counter) + " instance of " + character + " found in " + word)
     else:
-        print( str(counter) + " instances of " + character + " found in " + word)
+        print(str(counter) + " instances of " + character + " found in " + word)
 
 
 
