d76d93bf1b5499eade54cb443b8780249b7f3353
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-13 22:28:51 -0400

    eeks

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -7,6 +7,9 @@ secret: str = "python"
 word: str = input(f"What is your {len(secret)}-letter guess? ")
 counter: int = 0
 emoji: str = ""
+track_alt: int = 0
+existence: bool = False
+
 white_box: str = "\U00002B1C"
 green_box: str = "\U0001F7E9"
 yellow_box: str = "\U0001F7E8"
@@ -25,18 +28,37 @@ if len(secret) == len(word):
         print("Woo! You got it!")
         quit()
     else:
-        while counter < len(secret):
-            if word[counter] == secret[counter]:
-                emoji += green_box
-            else: 
-                emoji += white_box
-            counter = counter + 1
+        while existence is False and counter < len(secret):
+            if track_alt == counter:
+                existence: bool = True
+            else:
+                track_alt += 1
+        if existence is True:
+            emoji += yellow_box
+        else: 
+            emoji += white_box
         print(emoji)
         print("Not quite. Play again soon!")
         quit()
 
 
+if secret != word:
+    while existence is False and counter < len(secret):
+        if track_alt == counter:
+            existence: bool = True
+        else:
+            track_alt += 1
+    if existence is True:
+        emoji += yellow_box
+    else: 
+        emoji += white_box
 
 
 
 
+while counter < len(secret):
+            if word[counter] == secret[counter]:
+                emoji += green_box
+            else: 
+                emoji += white_box
+            counter = counter + 1
