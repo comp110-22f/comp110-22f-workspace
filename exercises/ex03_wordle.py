11c5cedc7222a564c9ade4caa99d32fc2518f387
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-15 00:56:05 -0400

    thanks

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -70,7 +70,7 @@ def main () -> None:
     """The entrypoint of the program and the main game loop."""
     secret: str = "codes"
     number: int = 5
-    tries = 1
+    tries: int = 1
     turn: str = (f"=== Turn {tries}/6 ===")
     guess: str = input(f"Enter a {number} character word: ")
     white_box: str = "\U00002B1C"
@@ -81,32 +81,35 @@ def main () -> None:
     while len(guess) != number:
         guess: str = input(f"That wasn't {number} chars! Try again: ")
     if len(guess) == number:
-        while tries < 6:
-            print(f"{turn}")
-            if guess == secret:
-                while counter < len(secret):
-                    if guess[counter] == secret[counter]:
-                        emoji += green_box
-                    counter = counter + 1
-                    print(f"{emoji}")
-                    print(f"You won in {tries}/6 turns!")
-            else:
-                while counter < len(secret):
-                    if guess[counter] == secret[counter]:
-                        emoji += green_box
-                    else:
-                        track: int = 0
-                        existence: bool = False
-                        while ((existence is False) & (track < len(secret))):
-                            if guess[counter] == secret[track]:
-                                existence = True
-                            else:
-                                track += 1
-                        if existence is True:
-                            emoji += yellow_box
+        if guess == secret:
+            while counter < len(secret):
+                if guess[counter] == secret[counter]:
+                    emoji += green_box
+                counter = counter + 1
+            print(f"{emoji}")
+            print(f"You won in {tries}/6 turns!")
+        else:
+            attempts: int = 1
+            turns: str = (f"=== Turn {attempts}/6 ===")
+            while counter < len(secret):
+                print(f"{turns}")
+                if guess[counter] == secret[counter]:
+                    emoji += green_box
+                else:
+                    track: int = 0
+                    existence: bool = False
+                    while ((existence is False) & (track < len(secret))):
+                        if guess[counter] == secret[track]:
+                            existence = True
                         else:
-                            emoji += white_box
+                            track += 1
+                    if existence is True:
+                        emoji += yellow_box
+                    else:
+                        emoji += white_box
                     counter += 1
                     track = 0
-                    tries = tries + 1
-                print(f"{emoji}")
+                    attempts = attempts + 1
+            print(f"{emoji}")
+            guess: str = input(f"Enter a {number} character word: ")
+
