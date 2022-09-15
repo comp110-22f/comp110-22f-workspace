e6e44bbfc8f46c02f1a0805626f4676bfcf04fd6
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-14 21:11:07 -0400

    eek

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -2,21 +2,25 @@
 
 __author__ = "730470086"
 
+counter: int = 0
+
 def contains_char (word: str , character: str) -> bool:
     """Returns True if character is in word."""
     assert len(character) == 1
     while counter < len(word):
-        track: int = 0
-        existence: bool = False
-        counter: int = 0
-        while ((existence is False) & (track < len(word))):
-            if word[counter] == character:
-                existence = True
-            else:
-                track += 1
-        if existence is True:
+        if word[counter] == character:
             return True
         else:
-            return False
-    counter += 1
-    track = 0
+            track: int = 0
+            existence: bool = False
+            while ((existence is False) & (track < len(word))):
+                if word[counter] == character:
+                    existence = True
+                else:
+                    track += 1
+            if existence is True:
+                return True
+            else:
+                return False
+        counter += 1
+        track_alt = 0
