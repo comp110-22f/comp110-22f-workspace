f070fbf7c57b94a61ffba27792a4940515614053
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-14 20:15:01 -0400

    d

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -4,10 +4,17 @@ __author__ = "730470086"
 
 counter: int = 0
 
-def contains_car (a: str = input("Enter a word") , b: str = input("Enter a single charaacter ")) -> bool:
+def contains_char (word: str = input("Enter a word ") , character: str = input("Enter a single charaacter ")) -> bool:
     """Returns True if character is in word."""
-    if a == b:
-        while counter < len(a):
-            return True
+    while counter < len(word):
+        track: int = 0
+        existence; bool = False
+        while ((existence is False) & (track < len(word))):
+            if word[counter] == character[counter]:
+                existence = True 
+            else:
+                track += 1
+    if existence is True:
+        return True
     else:
         return False
