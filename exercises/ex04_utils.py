43991cfec3e0bbd2351044cbf046a260ada779b3
Destiny Okonkwo <dezok@unc.edu>
2022-09-20 01:00:10 -0400

    Updating lesson plans and one exercise!

--- exercises/ex04_utils.py
+++ exercises/ex04_utils.py
@@ -1,17 +1,20 @@
-__author__ = "730574292"
-
 """Functions utilizing lists and commonly used functions."""
 
-def all(list_input: list[int], search_for: int) -> bool:
-    """Function takes a list of integers and an integers and searches through list for int. Returns True or False."""
+__author__ = "730574292"
 
+
+def all(list_input: list[int], search_for: int) -> bool:
+    """Function takes a list of integers and an integers and searches through list to check if all values are equal to int. Returns True or False."""
+    if len(list_input) == 0:
+        return False
+    
     idx: int = 0
     while idx < len(list_input):
-        if list_input[idx] == search_for:
-            return True
+        if list_input[idx] != search_for:
+            return False
         idx += 1
     
-    return False
+    return True
 
 
 def max(list_input: list[int]) -> int:
@@ -32,8 +35,10 @@ def max(list_input: list[int]) -> int:
 
 def is_equal(list_one: list[int], list_two: list[int]) -> bool:
     """Function takes two lists of integers and determines if all values of both lists are the same. Returns a boolean."""
-
     idx: int = 0
+
+    if len(list_one) != len(list_two):
+        return False
 
     while idx < len(list_one) and idx < len(list_two):
         if list_one[idx] != list_two[idx]:
