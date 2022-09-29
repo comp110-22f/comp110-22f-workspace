6ad69caa2a206d80356ab6341631364e636fd2fc
Haley Brosnahan <hbrosnahan@unc.edu>
2022-09-28 21:15:59 -0400

    yay

---
+++ exercises/ex05/utils.py
@@ -0,0 +1,34 @@
+"""EX05 - List Utility Functions."""
+
+__author__ = "730470086"
+
+def only_evens(xs: list[int]) -> list[int]:
+    """Returns list of only the even numbers in a list."""
+    new: list[int] = []
+    for number in xs:
+        if number % 2 == 0:
+            new.append(number)
+    return new
+
+def concat(a: list[int], b: list[int]) -> list[int]:
+    """Given two lists, returns concatination of the two."""
+    new: list[int] = []
+    for number in a:
+        new.append(number)
+    for item in b:
+        new.append(number)
+    return new
+
+def sub(xs: list[int], start: int, end: int) -> list[int]:
+    """Returrns subset of list."""
+    if xs == [] or start >= end:
+        return []
+    elif start < 0:
+        start = 0
+    elif end > len(xs):
+        end = len(xs)
+    new: list[int] = []
+    for number in range(start, end):
+        new.append(xs[number])
+    return new
+
