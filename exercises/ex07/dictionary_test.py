a414b4645e215b811ce1308b8e01b17810563a23
Haley Brosnahan <hbrosnahan@unc.edu>
2022-10-12 23:43:49 -0400

    yay

--- exercises/ex07/dictionary_test.py
+++ exercises/ex07/dictionary_test.py
@@ -1,3 +1,53 @@
 """EX07 - Tests For Dictionary."""
 
 __author__ = "730470086"
+
+from exercises.ex07.dictionary import invert
+from exercises.ex07.dictionary import favorite_color
+from exercises.ex07.dictionary import count
+import pytest
+
+
+with pytest.raises(KeyError):
+    my_dictionary = {'no': 'Haley', 'yes': 'Haley'}
+    invert(my_dictionary)
+
+
+def test_invert_edge() -> None:
+    """Tests for one entry."""
+    assert invert({'circle': 'round'}) == {'round': 'circle'}
+
+
+def test_invert_use_multiple() -> None:
+    """Tests if multiple entries are inverted correctly."""
+    assert invert({'pig': 'oink', 'cow': 'moo', 'cat': 'meow'}) == {'oink': 'pig', 'moo': 'cow', 'meow': 'cat'}
+
+
+def test_favorite_color_edge() -> None:
+    """Tests for one entry."""
+    assert favorite_color({'Haley': 'blue'}) == 'blue'
+
+
+def test_favorite_color_use_equal() -> None:
+    """Tests if two have the same number, it returns the first."""
+    assert favorite_color({'Haley': 'blue', 'Maya': 'blue', 'Addie': 'red', 'Katie': 'red'}) == 'blue'
+
+
+def test_favorite_color_use_multiple() -> None:
+    """Tests if multiple entries produces one color."""
+    assert favorite_color({'Haley': 'yellow', 'Maya': 'orange', 'Addie': 'red', 'Katie': 'orange'}) == 'orange'
+
+
+def test_count_edge() -> None:
+    """Tests for one entry."""
+    assert count(['apple']) == {'apple': 1}
+
+
+def test_count_use_numerous() -> None:
+    """Tests if works for numerous items in a list."""
+    assert count(['apple', 'banana', 'orange', 'mango']) == {'apple': 1, 'banana': 1, 'orange': 1, 'mango': 1}
+
+
+def test_count_use_multiple() -> None:
+    """Tests if works for multiple entries of item."""
+    assert count(['apple', 'apple', 'orange', 'apple', 'banana']) == {'apple': 3, 'orange': 1, 'banana': 1}
